3c9eba5f8cd8e30b67032963d9798bb8dabc2635
Tyrin Matthews <tyrinmatthews@Tyrins-MacBook-Air.local>
2025-10-23 17:34:40 -0400

    V. 1.0

---
+++ p.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define START 60
+#define END 0
+int main(void) {
+	
+	int n;
+	n = START;
+	while (n > END) {
+		printf("%d\n", n);
+		n--;
+		}
+	prinft("%s\n","Blastoff!");
+	}
