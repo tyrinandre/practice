beb39c1d9f1049a2208ba6d2e86e3adbc7cc817f
Tyrin Matthews <tyrinmatthews@Tyrins-MacBook-Air.local>
2025-10-23 22:04:57 -0400

    latest change

---
+++ p2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define START 100 
+#define END 0
+int main(void) {
+	
+	int n;
+	n = START;
+	while (n > END) {
+		printf("%d\n", n);
+		n--;
+		}
+	prinft("%s\n","Blastoff!");
+	}
